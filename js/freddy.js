5ed53def6fdf0a537eb2607f0acb8c7c36a3d250
Freddy <freddyliu1@gmail.com>
2013-09-11 17:50:41 +1000

    changed dummy data to include items described on ticket, have a basic list displaying the items

--- js/freddy.js
+++ js/freddy.js
@@ -1,52 +1,107 @@
+/*
+List all venues that meet the criteria.
+This should include the name, a short description.
+When this is clicked on more details should be expanded.
+
+This should include website, address, open hours/days, wine types, star rating.
+When clicked on again, collapse 
+
+*/
+
 var wineries = [
 	{ 
-		"name": "Hunterwine", 
+		"name": "Hunterwine",
+		"descriptionShort": "Best wine in Hunter Valley",
+		"website": "www.hunterwine.com.au",
+		"openHours": [
+				{
+					"monFri": "10:00-22:00",
+					"sat":	"11:00-23:00"
+				}
+		],
 		"location": [
 				{
 					"address": "hv road 2",
 					"region": "Hunter Valley"
 				}
 		],
-		"wine": "white red"
+		"wine": "white red",
+		"rating": 3
 	},
 	{
 		"name": "Sydney Empire",
+		"descriptionShort": "Sydney's King of Wines",
+		"website": "www.sydneyempirewine.com.au",
+		"openHours": [
+				{
+					"monFri": "10:00-22:00",
+					"sat":	"11:00-23:00"
+				}
+		],
 		"location": [
 				{
 					"address": "William Street",
 					"region": "Sydney"
 				}
 		],
-		"wine": "red"
+		"wine": "red",
+		"rating": 3
 	},
 	{
 		"name": "Freddy Intern",
+		"descriptionShort": "Intern wine, nice stuff",
+		"website": "www.internwine.com.au",
+		"openHours": [
+				{
+					"monFri": "10:00-22:00",
+					"sat":	"11:00-23:00"
+				}
+		],
 		"location": [
 				{
 					"address": "Hurstville way",
 					"region": "Hurstville"
 				}
 		],
-		"wine": "white"
+		"wine": "white",
+		"rating": 3
 	},
 	{
 		"name": "Jack Intern",
+		"descriptionShort": "Intern wine, nice stuff",
+		"website": "www.internwine.com.au",
+		"openHours": [
+				{
+					"monFri": "10:00-22:00",
+					"sat":	"11:00-23:00"
+				}
+		],
 		"location": [
 				{
 					"address": "Eastwood way",
 					"region": "Eastwood"
 				}
 		],
-		"wine": "white"
+		"wine": "white",
+		"rating": 3
 	},
 	{
 		"name": "Henry Wine",
+		"descriptionShort": "Intern wine, nice stuff",
+		"website": "www.internwine.com.au",
+		"openHours": [
+				{
+					"monFri": "10:00-22:00",
+					"sat":	"11:00-23:00"
+				}
+		],
 		"location": [
 				{
 					"address": "Maroubra Road",
 					"region": "Maroubra"
 				}
 		],
-		"wine": "white red"
+		"wine": "white red",
+		"rating": 3
 	}
 ];
