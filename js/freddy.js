ff6a5d11b1f011ea94656d81cff292ca237ff703
Freddy <freddyliu1@gmail.com>
2013-09-11 15:54:59 +1000

    renamed index

---
+++ js/freddy.js
@@ -0,0 +1,52 @@
+var wineries = [
+	{ 
+		"name": "Hunterwine", 
+		"location": [
+				{
+					"address": "hv road 2",
+					"region": "Hunter Valley"
+				}
+		],
+		"wine": "white red"
+	},
+	{
+		"name": "Sydney Empire",
+		"location": [
+				{
+					"address": "William Street",
+					"region": "Sydney"
+				}
+		],
+		"wine": "red"
+	},
+	{
+		"name": "Freddy Intern",
+		"location": [
+				{
+					"address": "Hurstville way",
+					"region": "Hurstville"
+				}
+		],
+		"wine": "white"
+	},
+	{
+		"name": "Jack Intern",
+		"location": [
+				{
+					"address": "Eastwood way",
+					"region": "Eastwood"
+				}
+		],
+		"wine": "white"
+	},
+	{
+		"name": "Henry Wine",
+		"location": [
+				{
+					"address": "Maroubra Road",
+					"region": "Maroubra"
+				}
+		],
+		"wine": "white red"
+	}
+];
